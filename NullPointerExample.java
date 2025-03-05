94e1e0d0a95d56719e3a612ef02710537d75e10b
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-05 23:00:17 +0530

    [Harman] ADD;program to generate the null pointer exception

---
+++ NullPointerExample.java
@@ -0,0 +1,23 @@
+public class NullPointerExample {
+    public static void generateException() {
+        String text = null;
+        System.out.println(text.length()); // This will throw NullPointerException
+    }
+
+    public static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Exception Handled: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Generating NullPointerException:");
+        // we can remove the // from the below method it see the nullpointer exception 
+        // generateException(); 
+        System.out.println("\nHandling NullPointerException:");
+        handleException();
+    }
+}
