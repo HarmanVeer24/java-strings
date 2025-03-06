61f20756f24f05596ca14cb2d03be413c0bbf5e6
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-06 22:51:04 +0530

    [Harman] ADD:program to check if given string is a palindrome

---
+++ PalindromeCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println(isPalindrome(str) ? "Palindrome" : "Not a Palindrome");
+    }
+    //helper method to check palindrome
+    public static boolean isPalindrome(String str) {
+        int start = 0, end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
